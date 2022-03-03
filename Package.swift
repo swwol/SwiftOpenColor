0d6212612dd4930d80326772f7632042c985d9a5
sam woolf <sam.woolf@gmail.com>
2022-03-03 11:05:56 +0000

    restrict platform to v11

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftOpenColor",
+    platforms: [.iOS(.v11), .macOS(.v11)],
     products: [
         .library(
             name: "SwiftOpenColor",
